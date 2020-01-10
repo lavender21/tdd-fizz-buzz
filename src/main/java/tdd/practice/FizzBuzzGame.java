634b7de45d145b7f44cb51dab364de2545193ce9
rtning <rtning@thoughtworks.com>
2020-01-10 16:26:31 +0800

    refactor mod 3, 5, 7 rules

--- src/main/java/tdd/practice/FizzBuzzGame.java
+++ src/main/java/tdd/practice/FizzBuzzGame.java
@@ -2,15 +2,19 @@ package tdd.practice;
 
 public class FizzBuzzGame {
     public String handleNumber(int num) {
-        if (num % 3 == 0) {
+        if (isMultiple(num, 3)) {
             return "Fizz";
         }
-        if (num % 5 == 0) {
+        if (isMultiple(num, 5)) {
             return "Buzz";
         }
-        if (num % 7 == 0) {
+        if (isMultiple(num, 7)) {
             return "Whizz";
         }
         return Integer.toString(num);
+    }
+
+    private boolean isMultiple(int num, int i) {
+        return num % i == 0;
     }
 }
