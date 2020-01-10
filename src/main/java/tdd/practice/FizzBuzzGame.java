31b3b92950ab0b4861658a43b0abb47c87a3073e
rtning <rtning@thoughtworks.com>
2020-01-10 16:23:03 +0800

    Add mod 3, 5, 7 rule for fizz buzz game

--- src/main/java/tdd/practice/FizzBuzzGame.java
+++ src/main/java/tdd/practice/FizzBuzzGame.java
@@ -2,6 +2,15 @@ package tdd.practice;
 
 public class FizzBuzzGame {
     public String handleNumber(int num) {
+        if (num % 3 == 0) {
+            return "Fizz";
+        }
+        if (num % 5 == 0) {
+            return "Buzz";
+        }
+        if (num % 7 == 0) {
+            return "Whizz";
+        }
         return Integer.toString(num);
     }
 }
--- src/test/java/tdd/practice/FizzBuzzGameTest.java
+++ src/test/java/tdd/practice/FizzBuzzGameTest.java
@@ -8,7 +8,27 @@ public class FizzBuzzGameTest {
 
     @Test
     public void should_return_1_when_input_1() {
-        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
-        assertThat(fizzBuzzGame.handleNumber(1)).isEqualTo("1");
+        assertFizzBuzzRule(1, "1");
     }
+
+    @Test
+    public void should_return_Fizz_when_input_3() {
+        assertFizzBuzzRule(3, "Fizz");
+    }
+
+    @Test
+    public void should_return_Buzz_when_input_5() {
+        assertFizzBuzzRule(5, "Buzz");
+    }
+
+    @Test
+    public void should_return_Whizz_when_input_7() {
+        assertFizzBuzzRule(7, "Whizz");
+    }
+
+    private void assertFizzBuzzRule(int num, String expected) {
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        assertThat(fizzBuzzGame.handleNumber(num)).isEqualTo(expected);
+    }
+
 }
