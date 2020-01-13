99e5d4b33d0dad66d28d670354fba5ee07b7952e
rtning <rtning@thoughtworks.com>
2020-01-13 17:24:49 +0800

    Refactor code to make it express the meaning

--- src/main/java/tdd/practice/FizzBuzzGame.java
+++ src/main/java/tdd/practice/FizzBuzzGame.java
@@ -2,6 +2,15 @@ package tdd.practice;
 
 public class FizzBuzzGame {
     public String say(int num) {
-        return "Fizz";
+        if (isContains(num, 3)){
+            if (!isContains(num, 5) || isContains(num, 7)) {
+                return "Fizz";
+            }
+        }
+        return null;
+    }
+
+    private boolean isContains(int num, int base) {
+        return String.valueOf(num).contains(String.valueOf(base));
     }
 }
