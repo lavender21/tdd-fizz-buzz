ac7c5546544ce47874bb623f401231bbbc754aea
rtning <rtning@thoughtworks.com>
2020-01-13 17:20:14 +0800

    Finish first rule when input contain3 say Fizz

---
+++ src/main/java/tdd/practice/FizzBuzzGame.java
@@ -0,0 +1,7 @@
+package tdd.practice;
+
+public class FizzBuzzGame {
+    public String say(int num) {
+        return "Fizz";
+    }
+}
---
+++ src/test/java/tdd/practice/FizzBuzzGameTest.java
@@ -0,0 +1,54 @@
+package tdd.practice;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+/*
+包含3 不包含5或
+包含3和7
+    全部输出Fizz
+    13 Fizz
+    30 Fizz
+    63 Fizz
+    37 Fizz
+    203 Fizz
+包含5 不包含7
+    5的倍数 Buzz
+    10 Buzz
+    7的倍数 Whizz
+
+    5，7的倍数 BuzzWhizz
+    35 BuzzWhizz
+
+包含7 不包含3
+    3的倍数 Fizz
+    7的倍数 Whizz
+    3，7的倍数 FizzWhizz
+
+不包含3，5，7，
+    3的倍数 Fizz
+    5的倍数 Buzz
+    7的倍数 Whizz
+    3，5的倍数 FizzBuzz
+    3，7的倍数 FizzWhizz
+    5，7的倍数 BuzzWhizz
+    3，5，7的倍数 FizzBuzzWhizz
+
+默认数字本身
+ */
+public class FizzBuzzGameTest {
+    @Test
+    public void return_Fizz_when_input_is_13_contain3_but_not_contain5() {
+        assertFizzBuzz(13, "Fizz");
+    }
+
+    @Test
+    public void return_Fizz_when_input_is_37_contain_3_and_7() {
+        assertFizzBuzz(37, "Fizz");
+    }
+
+    private void assertFizzBuzz(int input, String result) {
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        assertThat(fizzBuzzGame.say(input)).isEqualTo(result);
+    }
+}
