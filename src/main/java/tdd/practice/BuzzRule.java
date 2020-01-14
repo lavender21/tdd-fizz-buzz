404dac7aa4d39a705dc08c950aff392fe368af8d
rtning <rtning@thoughtworks.com>
2020-01-14 10:26:37 +0800

    refactor buzz rule

--- src/main/java/tdd/practice/BuzzRule.java
+++ src/main/java/tdd/practice/BuzzRule.java
@@ -4,13 +4,8 @@ public class BuzzRule extends Rule {
     private final String BUZZ = "Buzz";
 
     public String match(int num) {
-        if (isContains(num, BUZZ_NUM) && !isContains(num, WHIZZ_NUM)) {
-            if (isMultiple(num, BUZZ_NUM)) {
-                return BUZZ;
-            }
-        }
-        if(notContainsAllRuleNums(num) && isMultiple(num, BUZZ_NUM)) {
-            return BUZZ;
+        if (!isContains(num, WHIZZ_NUM) || notContainsAllRuleNums(num)) {
+            return isMultiple(num, BUZZ_NUM) ? BUZZ : "";
         }
         return "";
     }
