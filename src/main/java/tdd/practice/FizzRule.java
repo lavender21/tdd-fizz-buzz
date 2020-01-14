c0656e6b7e5d8b37241b9905ebd3ab84a11dbbf3
rtning <rtning@thoughtworks.com>
2020-01-14 10:47:58 +0800

    refactor fizz rule

--- src/main/java/tdd/practice/FizzRule.java
+++ src/main/java/tdd/practice/FizzRule.java
@@ -4,19 +4,10 @@ public class FizzRule extends Rule {
     private final String FIZZ = "Fizz";
 
     public String match(int num) {
-        if (isContains(num, FIZZ_NUM)){
-            if (!isContains(num, BUZZ_NUM) || isContains(num, WHIZZ_NUM)) {
-                return FIZZ;
-            }
-        }
-        if(isContains(num, WHIZZ_NUM) && !isContains(num, FIZZ_NUM)) {
-            if (isMultiple(num, FIZZ_NUM)) {
-                return FIZZ;
-            }
-        }
-        if(notContainsAllRuleNums(num) && isMultiple(num, FIZZ_NUM)) {
-            return FIZZ;
+        if (isContains(num, FIZZ_NUM) || isMultiple(num, FIZZ_NUM)){
+            return notContains(num, BUZZ_NUM) ? FIZZ : "";
         }
         return "";
     }
+
 }
